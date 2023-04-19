3316c38e5740b1bc0fc1e42ab64b14eb7ed5463a
saignan <saignankarrolla695@gmail.com>
2023-04-19 22:34:32 +0530

    Initial commit

---
+++ ArrayProblems2/ArrayProblems2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ArrayProblems2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Table of Contents\r\n" +
+                            "1) C# program to declare, initialize, input and print array elements\r\n" +
+                            "2) C# program to print all negative elements in array\r\n3) C# program to find sum of array elements\r\n" +
+                            "4) C# program to count negative elements in array\r\n" +
+                            "5) C# program to find maximum and minimum element in array\r\n" +
+                            "6) C# program to find second largest number in array\r\n" +
+                            "7) C# program to count even and odd elements in array\r\n" +
+                            "8) C# program to insert an element in array\r\n" +
+                            "9) C# program to delete element from an array\r\n" +
+                            "10) C# program to print all unique elements in array\r\n" +
+                            "11) C# program to delete duplicate elements from array\r\n" +
+                            "12) C# program to merge two sorted array\r\n" +
+                            "13) C# program to find reverse of array\r\n" +
+                            "14) C# program to search element in an array\r\n" +
+                            "15) C# program to sort array in ascending or descending order\r\n" +
+                            "16) C# program to add two matrices\r\n" +
+                            "17) C# program to subtract two matrices\r\n" +
+                            "18) C# program to multiply two matrices");
+            Console.WriteLine("Enter a value from 1 to 18:");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("hai");
+                    break;
+            }
+        }
+    }
+}
